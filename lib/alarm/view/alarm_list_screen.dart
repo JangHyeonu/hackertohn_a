4c79e626eee1a4e3cbd59984bbe257eea8029d62
Hyeonu <jhw3603@naver.com>
2023-12-26 23:03:44 +0900

    알람 리스트 없을 시 표출 문구 추가

--- lib/alarm/view/alarm_list_screen.dart
+++ lib/alarm/view/alarm_list_screen.dart
@@ -57,42 +57,51 @@ class AlarmListScreenState extends ConsumerState<AlarmListScreen> {
                 margin: const EdgeInsets.only(bottom: 16.0),
                 child: const KeywordComponent(),
               ),
+
               // 알림 목록 부분
+              state.isNotEmpty ?
               SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.6,
-                  child: ListView.builder(
-                    itemCount: state.length,
-                    itemBuilder: (context, index) {
-                      return Slidable(
-                        key: Key(state[index].alarmUid!),
-                        endActionPane: ActionPane(
-                          motion: const ScrollMotion(),
-                          children: [
-                            SlidableAction(
-                              // An action can be bigger than the others.
-                              flex: 2,
-                              onPressed: (context) {
-                                ref.read(alarmListProvider.notifier).removeAlarmById(state[index].alarmUid!);
-                              },
-                              backgroundColor: Theme.of(context).colorScheme.error,
-                              foregroundColor: Colors.white,
-                              icon: Icons.delete_forever_rounded,
-                              label: '알람 삭제',
-                            ),
-                          ],
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 4.0),
-                          child: AlarmListComponent(
-                            alarmUid: state[index].alarmUid,
-                            regDateTime: state[index].regDatetime,
-                            message: state[index].message,
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.6,
+                child: ListView.builder(
+                  itemCount: state.length,
+                  itemBuilder: (context, index) {
+                    return Slidable(
+                      key: Key(state[index].alarmUid!),
+                      endActionPane: ActionPane(
+                        motion: const ScrollMotion(),
+                        children: [
+                          SlidableAction(
+                            // An action can be bigger than the others.
+                            flex: 2,
+                            onPressed: (context) {
+                              ref.read(alarmListProvider.notifier).removeAlarmById(state[index].alarmUid!);
+                            },
+                            backgroundColor: Theme.of(context).colorScheme.error,
+                            foregroundColor: Colors.white,
+                            icon: Icons.delete_forever_rounded,
+                            label: '알람 삭제',
                           ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 4.0),
+                        child: AlarmListComponent(
+                          alarmUid: state[index].alarmUid,
+                          regDateTime: state[index].regDatetime,
+                          message: state[index].message,
                         ),
-                      );
-                    },
-                  )
+                      ),
+                    );
+                  },
+                )
+              )
+              :
+              Padding(
+                padding: const EdgeInsets.only(top: 200.0),
+                child: Center(
+                  child: Text("도착한 알림이 없습니다.")
+                ),
               ),
             ],
           ),
